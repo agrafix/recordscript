53e12d258d7b309764b72f9a2b5c226fb5bdeae4
Alexander Thiemann <mail@athiemann.net>
2018-10-31 21:12:09 -0700

    allow file input

--- app/Main.hs
+++ app/Main.hs
@@ -11,10 +11,17 @@ main :: IO ()
 main =
     do args <- getArgs
        case args of
+         ["-f", fileName] ->
+             T.readFile fileName >>= \expr ->
+             case compileCode expr of
+               Left (EParseError msg) -> T.putStrLn ("Parse error: \n" <> msg)
+               Left (ETypeError msg) -> T.putStrLn ("Type error: \n" <> msg)
+               Left (EFlowError msg) -> T.putStrLn ("Flow error: \n" <> msg)
+               Right ok -> T.putStrLn ok
          ["-e", expr] ->
              case compileCode (T.pack expr) of
                Left (EParseError msg) -> T.putStrLn ("Parse error: \n" <> msg)
                Left (ETypeError msg) -> T.putStrLn ("Type error: \n" <> msg)
                Left (EFlowError msg) -> T.putStrLn ("Flow error: \n" <> msg)
                Right ok -> T.putStrLn ok
-         _ -> putStrLn "Usage: -e 'expression'"
+         _ -> putStrLn "Usage: -e 'expression' OR -f 'filename'"
