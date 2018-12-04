151b65b6703ad6fc95dc0b2087dfd7123a6a4df0
Alexander Thiemann <mail@athiemann.net>
2018-12-03 23:47:45 -0800

    don't flow side effectful things

--- src/Optimize/FloatLet.hs
+++ src/Optimize/FloatLet.hs
@@ -6,6 +6,7 @@ import Analyse.VariableScopes
 import Types.Annotation
 import Types.Ast
 import Types.Common
+import Types.Types
 
 import Control.Monad
 import Data.Functor.Identity
@@ -18,51 +19,51 @@ import qualified Data.SortedList as SL
 
 import Debug.Trace
 
-data OpenLetEntry a
+data OpenLetEntry
     = OpenLetEntry
-    { ol_close :: Expr a -> Expr a
+    { ol_close :: Expr TypedPos -> Expr TypedPos
     , ol_bind :: Set Var
     , ol_free :: Set Var
     }
 
-instance Show (OpenLetEntry a) where
+instance Show (OpenLetEntry) where
     show ole =
         "OpenLetEntry { ol_close = undefined, ol_bind = "
         <> show (ol_bind ole) <> ", ol_free = " <> show (ol_free ole) <> "}"
 
-instance Eq (OpenLetEntry a) where
+instance Eq (OpenLetEntry) where
     (/=) ol1 ol2 =
         not (S.null (ol_bind ol1 `S.intersection` ol_free ol2))
 
-instance Ord (OpenLetEntry a) where
+instance Ord (OpenLetEntry) where
     compare ol1 ol2 =
         if not (S.null (ol_bind ol1 `S.intersection` ol_free ol2))
         then LT
         else GT
 
-data OpenLet a
+data OpenLet
     = OpenLet
-    { ol_stack :: SL.SortedList (OpenLetEntry a)
+    { ol_stack :: SL.SortedList OpenLetEntry
     } deriving (Show)
 
-emptyOpenLet :: OpenLet a
+emptyOpenLet :: OpenLet
 emptyOpenLet = OpenLet mempty
 
-combineOpenLet :: OpenLet a -> OpenLet a -> OpenLet a
+combineOpenLet :: OpenLet -> OpenLet -> OpenLet
 combineOpenLet ol1 ol2 =
     OpenLet
     { ol_stack = ol_stack ol1 <> ol_stack ol2
     }
 
-data SplitResult a
+data SplitResult
     = SplitResult
-    { sr_pass :: OpenLet a
+    { sr_pass :: OpenLet
       -- ^ safe to pass beyond the variable binding
-    , sr_apply :: OpenLet a
+    , sr_apply :: OpenLet
       -- ^ apply at current level
     }
 
-splitOpenLet :: S.Set Var -> OpenLet a -> SplitResult a
+splitOpenLet :: S.Set Var -> OpenLet -> SplitResult
 splitOpenLet boundVars openLet =
     let applySpan ole =
             not (S.null (boundVars `S.intersection` ol_free ole))
@@ -74,11 +75,11 @@ splitOpenLet boundVars openLet =
        , sr_apply = OpenLet $ SL.toSortedList apply
        }
 
-instance Monoid (OpenLet a) where
+instance Monoid OpenLet where
     mempty = emptyOpenLet
     mappend = combineOpenLet
 
-toOpenLet :: Monad m => a -> Let a -> m (OpenLet a, Expr a)
+toOpenLet :: Monad m => TypedPos -> Let TypedPos -> m (OpenLet, Expr TypedPos)
 toOpenLet a (Let boundVar@(Annotated _ v) boundExpr inVal) =
     do (boundOlRaw, boundExprRaw) <- floatLet boundExpr
        let split = splitOpenLet (S.singleton v) boundOlRaw
@@ -96,21 +97,21 @@ toOpenLet a (Let boundVar@(Annotated _ v) boundExpr inVal) =
            , inVal
            )
 
-applyOpenLet :: OpenLet a -> Expr a -> Expr a
+applyOpenLet :: OpenLet -> Expr TypedPos -> Expr TypedPos
 applyOpenLet ol expr =
     trace ("Apply: " <> show (F.toList (ol_stack ol))) $
     foldr apply expr (fmap ol_close $ F.toList $ ol_stack ol)
     where
       apply f e = f e
 
-freeList :: Monad m => a -> [Expr a] -> m (OpenLet a, Expr a)
+freeList :: Monad m => TypedPos -> [Expr TypedPos] -> m (OpenLet, Expr TypedPos)
 freeList ann vals =
     do fillVals <-
            fmap unzip $
            forM vals $ \val -> floatLet val
        pure (mconcat (fst fillVals), EList $ Annotated ann (snd fillVals))
 
-freeRecord :: Monad m => a -> Record (Expr a) -> m (OpenLet a, Expr a)
+freeRecord :: Monad m => TypedPos -> Record (Expr TypedPos) -> m (OpenLet, Expr TypedPos)
 freeRecord ann (Record hm) =
     do fillVals <-
            fmap unzip $
@@ -120,7 +121,7 @@ freeRecord ann (Record hm) =
        pure (mconcat (fst fillVals),
            ERecord $ Annotated ann $ Record $ HM.fromList (snd fillVals))
 
-freeRecordMerge :: Monad m => a -> RecordMerge a -> m (OpenLet a, Expr a)
+freeRecordMerge :: Monad m => TypedPos -> RecordMerge TypedPos -> m (OpenLet, Expr TypedPos)
 freeRecordMerge a rm =
     do (ol, tgt') <- floatLet (rm_target rm)
        (ols, mergeIns) <- unzip <$> mapM floatLet (rm_mergeIn rm)
@@ -129,7 +130,7 @@ freeRecordMerge a rm =
            , ERecordMerge $ Annotated a $ rm { rm_target = tgt', rm_mergeIn = mergeIns}
            )
 
-freeLambda :: Monad m => a -> Lambda a -> m (OpenLet a, Expr a)
+freeLambda :: Monad m => TypedPos -> Lambda TypedPos -> m (OpenLet, Expr TypedPos)
 freeLambda a lambda =
     do let boundHere = S.fromList $ map a_value (l_args lambda)
        (ol, body') <- floatLet (l_body lambda)
@@ -140,7 +141,7 @@ freeLambda a lambda =
              lambda { l_body = applyOpenLet (sr_apply split) body'}
            )
 
-freeBinOp :: Monad m => a -> BinOp a -> m (OpenLet a, Expr a)
+freeBinOp :: Monad m => TypedPos -> BinOp TypedPos -> m (OpenLet, Expr TypedPos)
 freeBinOp ann bo =
     case bo of
       BoAdd a b -> go BoAdd a b
@@ -164,7 +165,7 @@ freeBinOp ann bo =
              (ol2, r') <- floatLet r
              pure (ol1 <> ol2, EBinOp $ Annotated ann $ pack l' r')
 
-freeFunApp :: Monad m => a -> FunApp a -> m (OpenLet a, Expr a)
+freeFunApp :: Monad m => TypedPos -> FunApp TypedPos -> m (OpenLet, Expr TypedPos)
 freeFunApp ann fa =
     do (ol1, recv) <- floatLet (fa_receiver fa)
        (ols, args) <- unzip <$> mapM floatLet (fa_args fa)
@@ -173,7 +174,7 @@ freeFunApp ann fa =
            , EFunApp $ Annotated ann $ fa { fa_receiver = recv, fa_args = args}
            )
 
-freeIf :: Monad m => a -> If a -> m (OpenLet a, Expr a)
+freeIf :: Monad m => TypedPos -> If TypedPos -> m (OpenLet, Expr TypedPos)
 freeIf ann ifE =
     do (ols, bodies) <-
            fmap unzip $
@@ -187,7 +188,7 @@ freeIf ann ifE =
            , EIf $ Annotated ann $ ifE { if_bodies = bodies, if_else = else'}
            )
 
-freeCase :: Monad m => a -> Case a -> m (OpenLet a, Expr a)
+freeCase :: Monad m => TypedPos -> Case TypedPos -> m (OpenLet, Expr TypedPos)
 freeCase ann caseE =
     do (ol1, matchOn) <- floatLet (c_matchOn caseE)
        (ols, cases) <-
@@ -202,13 +203,29 @@ freeCase ann caseE =
            , ECase $ Annotated ann $ caseE { c_matchOn = matchOn, c_cases = cases }
            )
 
-floater :: Expr a -> Expr a
+freeLet :: Monad m => TypedPos -> Let TypedPos -> m (OpenLet, Expr TypedPos)
+freeLet ann letE@(Let boundVar@(Annotated _ v) boundExpr inVal) =
+    case t_eff (tp_type ann) of
+      SeIo ->
+          -- do not float side effectful things
+          do (boundOlRaw, boundExprRaw) <- floatLet boundExpr
+             let split = splitOpenLet (S.singleton v) boundOlRaw
+                 letExprFull =
+                     ELet $ Annotated ann $
+                     Let boundVar boundExprRaw inVal
+             pure (sr_pass split, applyOpenLet (sr_apply split) letExprFull)
+      _ ->
+          do (ol, e) <- toOpenLet ann letE
+             (ol2, e') <- floatLet e
+             pure (combineOpenLet ol ol2, e')
+
+floater :: Expr TypedPos -> Expr TypedPos
 floater expr =
     runIdentity $
     do (openLet, e) <- floatLet expr
        pure $ applyOpenLet openLet e
 
-floatLet :: Monad m => Expr a -> m (OpenLet a, Expr a)
+floatLet :: Monad m => Expr TypedPos -> m (OpenLet, Expr TypedPos)
 floatLet expr =
     case expr of
       ENative _ -> pure (emptyOpenLet, expr)
@@ -225,10 +242,7 @@ floatLet expr =
       EFunApp (Annotated x funAppE) -> freeFunApp x funAppE
       EIf (Annotated x ifE) -> freeIf x ifE
       ECase (Annotated x caseE) -> freeCase x caseE
-      ELet (Annotated x l) ->
-          do (ol, e) <- toOpenLet x l
-             (ol2, e') <- floatLet e
-             pure (combineOpenLet ol ol2, e')
+      ELet (Annotated x l) -> freeLet x l
       ECopy e ->
           do (floated, e') <- floatLet e
              pure (emptyOpenLet, ECopy $ applyOpenLet floated e')
